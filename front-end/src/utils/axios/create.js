31dc28ca123bc73792146c7cfd796076cf002f35
Vipul Sawant <vipssawant842@gmail.com>
2025-03-06 21:04:55 +0530

    updated src/utils/axios/create.js

--- front-end/src/utils/axios/create.js
+++ front-end/src/utils/axios/create.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000/api/v1";
+const baseURL = import.meta.env.VITE_BACKEND_URL || "https://simplenotesapp-h9bp.onrender.com/api/v1";
 
 const createClient = axios.create({
     baseURL,
