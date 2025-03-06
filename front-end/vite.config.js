7d9fc201480ef3427fecc9f696bf1d4d9ee5ddd1
Vipul Sawant <vipssawant842@gmail.com>
2025-03-06 20:57:03 +0530

    front-end/vite.config.js updated for github env secrets

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/SimpleNotesApp/", // Must match the GitHub repo name
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL),
+  },
 })
