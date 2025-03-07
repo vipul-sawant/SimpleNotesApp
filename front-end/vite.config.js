aae69fc1543f390bcd795cc90176465459b3adf0
Vipul Sawant <vipssawant842@gmail.com>
2025-03-07 17:35:19 +0530

    solution

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");  // Load all env variables
-
-  console.log("✅ Loaded VITE_BACKEND_URL:", env.VITE_BACKEND_URL); // Debugging
+export default defineConfig(({ _ }) => {
 
   return {
     plugins: [react()],
-    define: {
-      "import.meta.env.VITE_BACKEND_URL": JSON.stringify(env.VITE_BACKEND_URL),
-    },
 	base:"/SimpleNotesApp/"
   };
 });
