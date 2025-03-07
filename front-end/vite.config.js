50889b49a8829a7038b60232e0c711036da8afcd
Vipul Sawant <vipssawant842@gmail.com>
2025-03-07 17:00:47 +0530

    changed vite cofig

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/SimpleNotesApp/", // Must match the GitHub repo name
-  plugins: [react()]
+export default defineConfig(({mode})=>{
+	const env = loadEnv(mode, process.cwd(), "");
+	return {
+		
+		plugins: [react()],
+		define: {
+			"import.meta.env.VITE_BACKEND_URL": JSON.stringify(env.VITE_BACKEND_URL),
+		  },
+		base: "/SimpleNotesApp/", // Must match the GitHub repo name
+	}
 })
