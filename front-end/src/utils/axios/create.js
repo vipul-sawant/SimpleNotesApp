aafb38dfc5fb151222b2afdff6f34af88fef4957
Vipul Sawant <vipssawant842@gmail.com>
2025-03-07 15:12:05 +0530

    Testing

--- front-end/src/utils/axios/create.js
+++ front-end/src/utils/axios/create.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const baseURL = import.meta.env.VITE_BACKEND_URL || "https://simplenotesapp-h9bp.onrender.com/api/v1";
 
+console.log("baseURL :", baseURL);
+
 const createClient = axios.create({
     baseURL,
     withCredentials:true
