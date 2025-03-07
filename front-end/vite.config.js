35ba1c1d13078db0409d12fbfa9a25cc98b6bcca
Vipul Sawant <vipssawant842@gmail.com>
2025-03-07 14:50:49 +0530

    rebuild

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/SimpleNotesApp/", // Must match the GitHub repo name
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL),
-  },
+  plugins: [react()]
 })
